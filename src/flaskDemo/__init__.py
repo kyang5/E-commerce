09b717e437d283fca985925773a66700a3e29298
Olga Velichko <60892608+OVUSA@users.noreply.github.com>
2021-11-27 13:47:31 -0600

    added import

--- src/flaskDemo/__init__.py
+++ src/flaskDemo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://NAME:PASSWORD@localhost/company'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -13,3 +13,6 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from flaskDemo import routes
+from flaskDemo import models
+
+models.db.create_all()
